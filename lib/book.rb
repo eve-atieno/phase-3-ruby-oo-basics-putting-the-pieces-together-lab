ee63a5e6117e7aa7ecbface860163cf6174e4def
eve-atieno <atienoevelyne53@gmail.com>
2023-02-08 23:47:12 +0300

    completed assigment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+
+    attr_accessor :title, :author, :page_count, :genre
+        
+    def initialize(title)
+        @title = title
+        @page_count = 0
+        @author = ""
+   
+    end
+    
+    def turn_page
+        @page_count += 1
+        puts "Flipping the page...wow, you read fast!"
+    end
+  end
 
 
+  
